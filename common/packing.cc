9d139aee8d17054a11b9a1aa94fe863dd1d789fa
Sean Ogden <positivelyskewed.sf@gmail.com>
2013-02-11 04:46:29 -0500

    add OSX support.
    Signed-off-by: Sean Ogden <positivelyskewed.sf@gmail.com>

--- common/packing.cc
+++ common/packing.cc
@@ -50,6 +50,8 @@ replicant :: operator << (e::buffer::packer lhs, const po6::net::ipaddr& rhs)
         rhs.pack(&sa, 0);
 #ifdef _MSC_VER
         memmove(data, &sa.sin6_addr.u.Byte, 16);
+#elif defined __APPLE__
+        memmove(data, &sa.sin6_addr.__u6_addr.__u6_addr8, 16);
 #else
         memmove(data, &sa.sin6_addr.__in6_u.__u6_addr8, 16);
 #endif
@@ -88,6 +90,8 @@ replicant :: operator >> (e::unpacker lhs, po6::net::ipaddr& rhs)
         in6_addr ia;
 #ifdef _MSC_VER
         memmove(ia.u.Byte, rem.data(), 16);
+#elif defined __APPLE__
+        memmove(ia.__u6_addr.__u6_addr8, rem.data(), 16);
 #else
         memmove(ia.__in6_u.__u6_addr8, rem.data(), 16);
 #endif
