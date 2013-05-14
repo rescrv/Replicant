f2730e6df3192f542acb5a6f646f61129a9611d3
Veniamin Gvozdikov <g.veniamin@googlemail.com>
2013-05-14 16:17:05 +0400

    [FreeBSD]: fixes for freebsd

--- daemon/object_manager.cc
+++ daemon/object_manager.cc
@@ -60,7 +60,10 @@
 #include "daemon/object_manager.h"
 #include "daemon/replicant_state_machine.h"
 #include "daemon/replicant_state_machine_context.h"
-#if defined __APPLE__
+#if defined __APPLE__ || defined __FreeBSD__
+#ifdef __FreeBSD__
+#include <sys/stat.h>
+#endif
 #include "memstream.h"
 #endif
 
