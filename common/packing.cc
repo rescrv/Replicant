783d5597808837426892df978062f29c6749630a
bra-fsn <bra@fsn.hu>
2015-01-05 09:44:07 +0100

    Make this compile on FreeBSD too

--- common/packing.cc
+++ common/packing.cc
@@ -50,7 +50,7 @@ replicant :: operator << (e::buffer::packer lhs, const po6::net::ipaddr& rhs)
         rhs.pack(&sa, 0);
 #ifdef _MSC_VER
         memmove(data, &sa.sin6_addr.u.Byte, 16);
-#elif defined __APPLE__
+#elif defined __APPLE__ || defined __FreeBSD__
         memmove(data, &sa.sin6_addr.__u6_addr.__u6_addr8, 16);
 #else
         memmove(data, &sa.sin6_addr.__in6_u.__u6_addr8, 16);
@@ -90,7 +90,7 @@ replicant :: operator >> (e::unpacker lhs, po6::net::ipaddr& rhs)
         in6_addr ia;
 #ifdef _MSC_VER
         memmove(ia.u.Byte, rem.data(), 16);
-#elif defined __APPLE__
+#elif defined __APPLE__ || defined __FreeBSD__
         memmove(ia.__u6_addr.__u6_addr8, rem.data(), 16);
 #else
         memmove(ia.__in6_u.__u6_addr8, rem.data(), 16);
